f6e5249da4697f1f7906d6a6d94eb2a08aac1c50
Khoi Le <khoileee89@gmail.com>
2016-10-25 18:13:19 -0400

    data now prints to view

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let theOfficeCast =[
+  {name: "Michael Scott", title: "Supervisor", photo_url: "http://az616578.vo.msecnd.net/files/2015/08/24/6357600113572837231773916132_michael-scott-s-top-tantrums.png"},
+  {name: "Dwight Schrute", title: "Salesman", photo_url: "https://img.buzzfeed.com/buzzfeed-static/static/2015-12/16/11/campaign_images/webdr15/33-dwight-schrute-jokes-that-still-make-you-laugh-2-11189-1450281904-8_dblbig.jpg"},
+  {name: "Toby Flenderson", title: "HR Rep", photo_url: "https://lh3.googleusercontent.com/-Xx3ekrHllIU/AAAAAAAAAAI/AAAAAAAAABU/62LVGMcOaqI/photo.jpg"}
+]
+
+// (function(){
+//   angular.module("wdinstagram", [
+//
+//   ])
+// })();
+
+angular
+  .module("wdinstagramApp", [])
+  .controller("WdinstagramController", [ WdinstagramControllerFunction])
+
+  function WdinstagramControllerFunction (){
+    this.characters = theOfficeCast
+  }
